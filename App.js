8e7c1b642608df4380fa096d6a1eb68a6de06372
bhanu singh <bhanu2795@gmail.com>
2019-05-08 13:24:27 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import Navigator from './src/routes/routing';
+
+export default class App extends Component {
+  render() {
+    return <Navigator />
+  }
+}
